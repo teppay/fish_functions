e2aaeee3951cfdbc5682f10e136b82cbe709aecd
teppay <teppay.sec@gmail.com>
2018-11-06 15:14:47 +0900

    add fish_prompt.fish

---
+++ fish_prompt.fish
@@ -0,0 +1,26 @@
+function fish_prompt --description 'Write out the prompt'
+	set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
+    set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/")
+    set -l dirs (echo $pwd | string sub -s 3 | string split /)
+    set -l max_len 65
+    set -l idx 1
+    while [ (string length '~/'(string join / $dirs)) -gt $max_len ]
+        set dirs[$idx] '('(string sub -l 3 $dirs[$idx])')'
+        set idx (math $idx+1)
+        set pwd '~/'(string join / $dirs)
+    end
+
+    set -l prompt_symbol ''
+    set -x fish_color_cwd ff8b26
+    set -x fish_color_param blue
+
+    switch "$USER"
+        case root toor
+            set prompt_symbol '#'
+        case '*'
+            set prompt_symbol '$'
+    end
+
+    echo -s "[$USER" @ (prompt_hostname) ' ' (set_color $fish_color_cwd) $pwd (set_color normal) ']' (__fish_git_prompt)
+    echo -n "$prompt_symbol > "
+end

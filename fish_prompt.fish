6bb8bc1668a9e7ae4bdec86a6e332a8b61b48e74
teppay <teppay.sec@gmail.com>
2019-03-17 19:40:15 +0900

    update prompt

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -2,9 +2,10 @@ function fish_prompt --description 'Write out the prompt'
 	set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
     set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/")
     set -l dirs (echo $pwd | string sub -s 3 | string split /)
-    set -l max_len 65
+    set -l width 110
+    set -l max_len (math $width-(string length (string join "" "[$USER" @ (prompt_hostname) ' ]' (__fish_git_prompt))))
     set -l idx 1
-    
+
     for i in (seq (count $dirs))
         if [ (string length $pwd) -le $max_len ]
             break
@@ -13,10 +14,8 @@ function fish_prompt --description 'Write out the prompt'
         set idx (math $idx+1)
         set pwd '~/'(string join / $dirs)
     end
-    
+
     set -l prompt_symbol ''
-    set -x fish_color_cwd ff8b26
-    set -x fish_color_param blue
 
     switch "$USER"
         case root toor
@@ -25,6 +24,6 @@ function fish_prompt --description 'Write out the prompt'
             set prompt_symbol '$'
     end
 
-    echo -s "[$USER" @ (prompt_hostname) ' ' (set_color $fish_color_cwd) $pwd (set_color normal) ']' (__fish_git_prompt)
-    echo -n "$prompt_symbol > "
+    echo -s "╭─ ["(set_color $fish_color_cwd)  $pwd (set_color normal) ']' (__fish_git_prompt)
+    echo -sn  "╰─> " (set_color normal)
 end
